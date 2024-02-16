5a0936359658dccde95a7c69da8bc079954636a7
maxpalmgren <51370310+maxpalmgren@users.noreply.github.com>
2024-02-16 07:48:41 +0100

    small fix

--- src/components/GameOne.tsx
+++ src/components/GameOne.tsx
@@ -4,39 +4,32 @@ import StageData from "../assets/stages/stages.json";
 import { Stage } from "../Types/Stage";
 
 type Props = {
-    playerOne: string
-    playerTwo: string
-}
-
-
+  playerOne: string;
+  playerTwo: string;
+};
 
 const GameOne: FC<Props> = ({ playerOne, playerTwo }) => {
+  const randomizeFirstBanner = () => {
+    const random = Math.random();
+    if (random > 0.5) {
+      return playerOne;
+    } else return playerTwo;
+  };
 
-    const randomizeFirstBanner = () => {
-        const random = Math.random()
-        if (random > 0.5) {
-            return playerOne
-        }
-        else return playerTwo
-    }
+  const stages = StageData as Stage[];
 
-    const stages = StageData as Stage[];
+  return (
+    <div>
+      <div className="flex justify-center mb-4">
+        {randomizeFirstBanner()} starts to ban 4 maps
+      </div>
+      <div className="flex gap-2 flex-wrap justify-center">
+        {stages.map((stage) => (
+          <StageCard stage={stage} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    return (
-
-        <div>
-            <div className="flex justify-center mb-4">{randomizeFirstBanner()} starts to ban 4 maps</div>
-            <div className="flex gap-2 flex-wrap justify-center">
-                <StageCard stage={stages[0]} />
-                <StageCard stage={stages[1]} />
-                <StageCard stage={stages[2]} />
-                <StageCard stage={stages[3]} />
-                <StageCard stage={stages[4]} />
-                <StageCard stage={stages[5]} />
-                <StageCard stage={stages[6]} />
-            </div>
-        </div>
-    )
-}
-
-export default GameOne
+export default GameOne;
