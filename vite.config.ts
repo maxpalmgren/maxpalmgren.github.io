381429af021bc626d0b4aff110e9c4b5589ec4bf
maxpalmgren <51370310+maxpalmgren@users.noreply.github.com>
2025-03-10 21:59:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/[maxpalmgren.github.io]/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
